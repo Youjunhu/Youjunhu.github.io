7c2aa04ff5a29884fe4a8327ed6ea82201f351c5
youjunhu <youjunhu@gmail.com>
2023-03-27 09:52:23 +0800

    update fourier

--- research_notes/fourier_analysis/p.sh
+++ research_notes/fourier_analysis/p.sh
@@ -1,1 +1,1 @@
-htlatex nonlinear_gyrokinetic_equation.tex "myconfig, xhtml,4, charset=utf-8" " -cunihtf -utf8"  
+htlatex fourier_analysis.tex "myconfig, xhtml,4, charset=utf-8" " -cunihtf -utf8"  
