e019eaa7d2b00f28a210450a735243bb7ecb59e6
youjunhu <youjunhu@gmail.com>
2023-05-22 17:21:17 +0800

    update fourier

--- research_notes/fourier_analysis/p.sh
+++ research_notes/fourier_analysis/p.sh
@@ -1,1 +1,2 @@
-htlatex fourier_analysis.tex "myconfig, xhtml,4, charset=utf-8" " -cunihtf -utf8"  
+#htlatex fourier_analysis.tex "myconfig, xhtml,4, charset=utf-8" " -cunihtf -utf8"
+htlatex fourier_analysis.tex "myconfig, xhtml,3, charset=utf-8" " -cunihtf -utf8"  
