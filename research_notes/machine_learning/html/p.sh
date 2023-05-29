7f8b94c1f32e313731dc623964c455074ba30dd7
youjunhu <youjunhu@gmail.com>
2023-05-29 14:25:01 +0800

    update my note on machine learning

---
+++ research_notes/machine_learning/html/p.sh
@@ -0,0 +1,3 @@
+htlatex machine_learning.tex "myconfig, xhtml, 2, charset=utf-8" " -cunihtf -utf8"
+
+#rsync -avz --delete /home/yj/theory/guiding_center_motion/html/ /home/yj/Youjunhu.github.io/research_notes/guiding_center_motion
