251e6d3db7de01318883dfbf6cbca660a7cb1a33
youjunhu <youjunhu@gmail.com>
2023-05-29 13:35:59 +0800

    update my note on guiding-center motion

--- research_notes/guiding_center_motion/p.sh
+++ research_notes/guiding_center_motion/p.sh
@@ -1,1 +1,3 @@
-htlatex ../tex/guiding_center_motion.tex "myconfig, xhtml, 2, charset=utf-8" " -cunihtf -utf8"  
+htlatex ../tex/guiding_center_motion.tex "myconfig, xhtml, 2, charset=utf-8" " -cunihtf -utf8"
+
+#rsync -avz --delete /home/yj/theory/guiding_center_motion/html/ /home/yj/Youjunhu.github.io/research_notes/guiding_center_motion
