56f07756fa42c77fd01bb49f59e8cfbfc4a47266
youjunhu <youjunhu@gmail.com>
2023-06-01 10:25:01 +0800

    update my note on gyrokinetic

--- research_notes/nonlinear_gyrokinetic_equation_html/p.sh
+++ research_notes/nonlinear_gyrokinetic_equation_html/p.sh
@@ -1,10 +1,11 @@
 mainfilename=nonlinear_gyrokinetic_equation
-texmacs -c ../${mainfilename}.tm ${mainfilename}.pdf -quit 
-texmacs -c ../${mainfilename}.tm ${mainfilename}.tex -quit 
+#texmacs -c ../${mainfilename}.tm ${mainfilename}.pdf -quit 
+#texmacs -c ../${mainfilename}.tm ${mainfilename}.tex -quit 
 #pdflatex ${mainfilename}.tex
 #pdflatex ${mainfilename}.tex
+
 htlatex ${mainfilename}.tex "myconfig, xhtml,2, charset=utf-8" " -cunihtf -utf8"
 cp ${mainfilename}.html index.html
-#rsync -avz --delete ../html/ /home/yj/Youjunhu.github.io/research_notes/nonlinear_gyrokinetic_equation_html/
+rsync -avz --delete ../html/ /home/yj/Youjunhu.github.io/research_notes/nonlinear_gyrokinetic_equation_html/
 
 #~/bin/publish_html.sh to_cluster
