8805fd57ce153e2da52c2e5c24856a41d2fd015a
Youjunhu <youjunhu@gmail.com>
2026-02-03 22:35:01 +0800

    update gk

---
+++ research_notes/gk_html/p.sh
@@ -0,0 +1,11 @@
+mainfilename=nonlinear_gyrokinetic_equation
+#texmacs -c ../${mainfilename}.tm ${mainfilename}.pdf -quit 
+#texmacs -c ../${mainfilename}.tm ${mainfilename}.tex -quit 
+#pdflatex -shell-escape ${mainfilename}.tex
+#pdflatex -shell-escape ${mainfilename}.tex
+
+#htlatex ../${mainfilename}.tex "myconfig, xhtml, 3, charset=utf-8" " -cunihtf -utf8"
+cp ${mainfilename}.html index.html
+rsync -avz --delete ../html/ /home/yj/Youjunhu.github.io/research_notes/gk_html/
+
+#~/bin/publish_html.sh to_cluster
